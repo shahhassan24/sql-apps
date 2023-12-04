25ee909ce79dac7d664d69218709eba8d75c5d85
Shah Hassan <61498968+shady2498@users.noreply.github.com>
2023-12-04 22:58:17 +0500

    Update api.js

--- ingredients/api.js
+++ ingredients/api.js
@@ -49,7 +49,7 @@ router.get("/search", async (req, res) => {
 
   if (term) {
     where_clause = `WHERE CONCAT(title, type) ILIKE $2`;
-    params.push(item);
+    params.push(`%${term}%`);
   }
 
   const { rows } = await pool.query(
